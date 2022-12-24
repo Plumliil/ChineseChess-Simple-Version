d9b0ca8e7cd8a271da74d9cbb5d507e53b2f1cf5
plumliil <476453574@qq.com>
2022-12-24 13:57:09 +0800

    feat: ai

---
+++ assets/Script/play/AI.ts
@@ -0,0 +1,12 @@
+class AI {
+  private MIN_SCORE = -9999;
+  private MAX_SCORE = 9999;
+  private eatStack = [];
+  constructor() {}
+  createNew() {}
+  findBastNextStep() {}
+  search() {}
+  evaluate() {}
+  getPossibleStepFrom() {}
+  getAllPossibleStep() {}
+}
